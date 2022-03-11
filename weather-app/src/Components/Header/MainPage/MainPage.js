a5986d7e9670923487ffb1a3a6b22b393e1bf975
Aquib Yazdani <aquib.yazdani@harnstech.com>
2022-03-11 16:43:55 +0530

    cards created

---
+++ weather-app/src/Components/Header/MainPage/MainPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import WeatherCards from "../CardsSlide/WeatherCards";
+export default function MainPage() {
+  return (
+    <div>
+      <WeatherCards />
+    </div>
+  );
+}
